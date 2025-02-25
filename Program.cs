85599b5910eb3df760f6f8c3417ad4fdf513ed07
Sara-Hartuv <sarahartuv@gmail.com>
2025-02-25 14:57:15 +0200

    adding program file

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore.Storage;
+using Mock;
+using Repository.Interfaces;
+using Serviece.Services;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//di -database
+builder.Services.AddDbContext<IContext, DataBase>();
+
+//di
+builder.Services.AddServiceExtension();
+
+// enable cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "_myAllowSpecificOrigins",
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                      });
+});
+// enable cors
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+// enable cors
+app.UseCors("_myAllowSpecificOrigins");
+// enable cors
+
+app.MapControllers();
+
+app.Run();
+
